ae0cbdbf45c7cad1e4d4edce0acdb46490182001
Ankan Biswas <ankanbiswas365@gmail.com>
2025-04-09 22:17:35 +0530

    changed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
     build: {
-        outDir: 'dist', // Change 'dist' to your desired output directory
+        outDir: 'build', // Change 'dist' to your desired output directory
     },
     base: '/',
     plugins: [react(),],
